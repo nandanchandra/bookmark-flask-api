590b7a1e9cad577aeb479353f9bb343046a2ce11
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2021-08-26 22:48:41 +0530

    generateNewAccessToken

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
-import os
+from flask_jwt_extended import JWTManager
 
+import os
 
 from src.auth import auth
 from src.bookmarks import bookmarks
@@ -19,7 +20,7 @@ def create_app(test_config=None):
             SECRET_KEY=os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
-
+            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY'),
 
         )
 
@@ -29,6 +30,8 @@ def create_app(test_config=None):
 
     db.app=app
     db.init_app(app)
+
+    JWTManager(app)
 
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
--- src/auth.py
+++ src/auth.py
@@ -2,9 +2,12 @@ import validators
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from flask import Blueprint,jsonify,request
+from flask_jwt_extended import jwt_required, create_access_token, create_refresh_token, get_jwt_identity
 
 from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
 from src.database import User, db
+
+
 
 
 auth = Blueprint("auth",__name__,url_prefix="/api/v1/auth")
@@ -47,6 +50,46 @@ def register():
 
     }), HTTP_201_CREATED
 
-@auth.get("/me")
-def me():
-    return {"message":"Hello Me"}
+
+@auth.post('/login')
+def login():
+    email = request.json.get('email', '')
+    password = request.json.get('password', '')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        is_pass_correct = check_password_hash(user.password, password)
+
+        if is_pass_correct:
+            refresh = create_refresh_token(identity=user.id)
+            access = create_access_token(identity=user.id)
+
+            return jsonify({
+                'user': {
+                    'refresh': refresh,
+                    'access': access,
+                    'username': user.username,
+                    'email': user.email
+                }
+
+            }), HTTP_200_OK
+
+    return jsonify({'error': 'Wrong credentials'}), HTTP_401_UNAUTHORIZED
+
+
+
+# @auth.get("/me")
+# def me():
+#     return {"message":"Hello Me"}
+
+
+@auth.get('/token/refresh')
+@jwt_required(refresh=True)
+def refresh_users_token():
+    identity = get_jwt_identity()
+    access = create_access_token(identity=identity)
+
+    return jsonify({
+        'access': access
+    }), HTTP_200_OK
