503d9a79abcf273841a45e138b9e16c59dc2c13c
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2021-08-26 21:30:31 +0530

    registerUser

--- src/auth.py
+++ src/auth.py
@@ -1,11 +1,51 @@
-from flask import Blueprint
+import validators
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flask import Blueprint,jsonify,request
+
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
+from src.database import User, db
+
 
 auth = Blueprint("auth",__name__,url_prefix="/api/v1/auth")
 
 @auth.post('/register')
 def register():
-    return {"message":"Hello"}
+    username = request.json['username']
+    email = request.json['email']
+    password = request.json['password']
 
+    if len(password) < 6:
+        return jsonify({'error': "Password is too short"}), HTTP_400_BAD_REQUEST
+
+    if len(username) < 3:
+        return jsonify({'error': "User is too short"}), HTTP_400_BAD_REQUEST
+
+    if not username.isalnum() or " " in username:
+        return jsonify({'error': "Username should be alphanumeric"}), HTTP_400_BAD_REQUEST
+
+    if not validators.email(email):
+        return jsonify({'error': "Email is not valid"}), HTTP_400_BAD_REQUEST
+
+    if User.query.filter_by(email=email).first() is not None:
+        return jsonify({'error': "Email is taken"}), HTTP_409_CONFLICT
+
+    if User.query.filter_by(username=username).first() is not None:
+        return jsonify({'error': "username is taken"}), HTTP_409_CONFLICT
+
+    pwd_hash = generate_password_hash(password)
+
+    user = User(username=username, password=pwd_hash, email=email)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({
+        'message': "User created",
+        'user': {
+            'username': username, "email": email
+        }
+
+    }), HTTP_201_CREATED
 
 @auth.get("/me")
 def me():
