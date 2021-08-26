52f741983e6269d195758666f2a01e2277053bb9
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2021-08-27 00:11:30 +0530

    paginations

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,12 +1,78 @@
-from flask import Blueprint
+import validators
+
+from flask import Blueprint,request,jsonify
+from flask_jwt_extended import get_jwt_identity, jwt_required
+
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
+
+from src.database import Bookmark,db
 
 bookmarks = Blueprint("bookmarks",__name__,url_prefix="/api/v1/bookmarks")
 
-@bookmarks.post('/register')
-def register():
-    return {"message":"Hello"}
 
+@bookmarks.route('/', methods=['POST', 'GET'])
+@jwt_required()
+def handle_bookmarks():
+    current_user = get_jwt_identity()
 
-@bookmarks.get("/me")
-def me():
-    return {"message":"Hello Me"}
+    if request.method == 'POST':
+
+        body = request.get_json().get('body', '')
+        url = request.get_json().get('url', '')
+
+        if not validators.url(url):
+            return jsonify({
+                'error': 'Enter a valid url'
+            }), HTTP_400_BAD_REQUEST
+
+        if Bookmark.query.filter_by(url=url).first():
+            return jsonify({
+                'error': 'URL already exists'
+            }), HTTP_409_CONFLICT
+
+        bookmark = Bookmark(url=url, body=body, user_id=current_user)
+        db.session.add(bookmark)
+        db.session.commit()
+
+        return jsonify({
+            'id': bookmark.id,
+            'url': bookmark.url,
+            'short_url': bookmark.short_url,
+            'visit': bookmark.visits,
+            'body': bookmark.body,
+            'created_at': bookmark.created_at,
+            'updated_at': bookmark.updated_at,
+        }), HTTP_201_CREATED
+
+    else:
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 5, type=int)
+
+        bookmarks = Bookmark.query.filter_by(
+            user_id=current_user).paginate(page=page, per_page=per_page)
+
+        data = []
+
+        for bookmark in bookmarks.items:
+            data.append({
+                'id': bookmark.id,
+                'url': bookmark.url,
+                'short_url': bookmark.short_url,
+                'visit': bookmark.visits,
+                'body': bookmark.body,
+                'created_at': bookmark.created_at,
+                'updated_at': bookmark.updated_at,
+            })
+
+        meta = {
+            "page": bookmarks.page,
+            'pages': bookmarks.pages,
+            'total_count': bookmarks.total,
+            'prev_page': bookmarks.prev_num,
+            'next_page': bookmarks.next_num,
+            'has_next': bookmarks.has_next,
+            'has_prev': bookmarks.has_prev,
+
+        }
+
+        return jsonify({'data': data, 'meta':meta}), HTTP_200_OK
